32d02cea8c8136b0b9d0356964ce06158c5d4192
antoniogisondi <anto.gisondi@tiscali.it>
2023-09-05 20:22:53 +0200

    Create route, migration, controller, api ecc...

--- database/migrations/2023_09_05_175539_create_leads_table.php
+++ database/migrations/2023_09_05_175539_create_leads_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('mails', function (Blueprint $table) {
+        Schema::create('leads', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('mails');
+        Schema::dropIfExists('leads');
     }
 };
