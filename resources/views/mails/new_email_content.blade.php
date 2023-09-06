5d59900f5a1cc8d4c5024a25512be6c25b97c97d
antoniogisondi <anto.gisondi@tiscali.it>
2023-09-06 21:07:49 +0200

    Fixed email

---
+++ resources/views/mails/new_email_content.blade.php
@@ -0,0 +1,11 @@
+<h1>Nuova mail da form di <strong>Artigiano Edile Matera Riccardo</strong></h1>
+<div>
+    <h4>Nome:</h4><br>
+    <p>{{ $lead->name }}</p><br>
+    <br>
+    <h4>Email:</h4><br>
+    <p>{{ $lead->email }}</p><br>
+    <br>
+    <h4>Contenuto:</h4><br>
+    <p>{{ $lead->description }}</p>
+</div>
