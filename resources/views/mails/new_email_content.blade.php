29a5673f94c0a1a8fcb1db5f1ce6a69fef17d340
antoniogisondi <anto.gisondi@tiscali.it>
2023-09-07 12:58:03 +0200

    Fixed email pages graphics

--- resources/views/mails/new_email_content.blade.php
+++ resources/views/mails/new_email_content.blade.php
@@ -1,11 +1,12 @@
-<h1>Nuova mail da form di <strong>Artigiano Edile Matera Riccardo</strong></h1>
+<h1>Nuova mail da form di <strong style="color: #555555;">Artigiano Edile Matera Riccardo</strong></h1>
+
 <div>
-    <h4>Nome:</h4><br>
-    <p>{{ $lead->name }}</p><br>
-    <br>
-    <h4>Email:</h4><br>
-    <p>{{ $lead->email }}</p><br>
-    <br>
-    <h4>Contenuto:</h4><br>
-    <p>{{ $lead->description }}</p>
+    <h4 style="margin: 10px 0 5px; color: #333333;">Nome:</h4>
+    <p style="margin: 5px 0; color: #555555;">{{ $lead->name }}</p>
+
+    <h4 style="margin: 10px 0 5px; color: #333333;">Email:</h4>
+    <p style="margin: 5px 0; color: #555555;">{{ $lead->email }}</p>
+
+    <h4 style="margin: 10px 0 5px; color: #333333;">Contenuto:</h4>
+    <p style="margin: 5px 0; color: #555555;">{{ $lead->description }}</p>
 </div>
