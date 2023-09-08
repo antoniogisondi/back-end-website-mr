d9e6c00141a95fd48af1361d030a358bbc71b432
antoniogisondi <anto.gisondi@tiscali.it>
2023-09-08 17:10:34 +0200

    Create api for index and for show

--- app/Http/Controllers/Api/WorkController.php
+++ app/Http/Controllers/Api/WorkController.php
@@ -8,5 +8,29 @@ use App\Models\Work;
 
 class WorkController extends Controller
 {
-    
+    public function index(){
+        $works = Work::all();
+        return response()->json([
+            'success'  => true,
+            'results'  => $works
+        ]);
+    }
+
+    public function show($slug){
+        $work = Work::all()->where('slug', $slug)->first();
+
+        if($work){
+            return response()->json([
+                'success' => true,
+                'results' => $work
+            ]);
+        }
+        else{
+            return response()->json([
+                'success' => false,
+                'error' => 'Nessun lavoro trovato'
+            ]);
+        }
+    }
 }
+
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LeadController as LeadController;
+use App\Http\Controllers\Api\WorkController as WorkController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::post('/contacts', [LeadController::class, 'store']);
+Route::get('/works', [WorkController::class, 'index']);
+Route::get('/works/{slug}', [WorkController::class, 'show']);
